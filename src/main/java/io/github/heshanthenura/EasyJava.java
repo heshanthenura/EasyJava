b807579cad8154d61f341f728d11d80e81f14cae
Heshan Thenura <heshanthenura@protonmail.com>
2024-03-01 20:40:02 +0530

    *

---
+++ src/main/java/io/github/heshanthenura/EasyJava.java
@@ -0,0 +1,22 @@
+package io.github.heshanthenura;
+
+public class EasyJava {
+
+    /**
+     * The main method of the program.
+     *
+     * @param args The command-line arguments passed to the program.
+     */
+    public static void main(String[] args) {
+        printStr("asdasd");
+    }
+
+    /**
+     * Prints the given string.
+     *
+     * @param str The string to be printed.
+     */
+    static void printStr(String str){
+        System.out.println(str);
+    }
+}
